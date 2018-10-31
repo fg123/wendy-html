dbe69bfef80091051d371fb84a8abd8180b08167
Felix Guo <felixguoxiuping@gmail.com>
2018-10-31 01:58:51 -0400

    wendy-html: bring back mobile scaling

--- static/main.js
+++ static/main.js
@@ -94,3 +94,14 @@ function determineAnchorActive(scrollPosition) {
         activeIndex = newIndex;
     }
 }
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+    var ww = window.innerWidth; //get proper width
+    var mw = 500; // min width of site
+    var ratio = ww / mw; //calculate ratio
+    if (ww < mw) { //smaller than minimum size
+        document.querySelector('meta[name=viewport]').content = 'initial-scale=' + ratio + ', maximum-scale=' + ratio + ', minimum-scale=' + ratio + ', user-scalable=no, width=' + ww;
+    } else { //regular size
+        document.querySelector('meta[name=viewport]').content = 'initial-scale=1.0, maximum-scale=2, minimum-scale=1.0, user-scalable=no, width=' + ww;
+    }
+}
